06808592ce2493afbef82c11dd29f62f3128079b
Marti3647 <89570305+Marti3647@users.noreply.github.com>
2023-02-01 09:49:14 +0100

    Initial commit

---
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
@@ -0,0 +1,67 @@
+package oslomet.testing;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpSession;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestSikkerhetsController {
+
+    @InjectMocks
+    // denne skal testes
+    private Sikkerhet sikkerhetsController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private MockHttpSession session;
+
+/* Hentet fra video, vet ikke om skal være med!
+
+    @Before
+    public void initSession(){
+        Map<String, Object> attributes = new HashMap<~>();
+
+        doAnswer(new Answer<Object>(){
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable{
+                String key = (String) invocation.getArgument()[0];
+                Object value = invocation.getArgument()[1];
+                attributes.put(key, value);
+                return null;
+            }
+        }).when(session).setAttribute(anyString(), any());
+    }
+*/
+
+    @Test
+    public void test_sjekkLoggetInn() {
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("OK");
+
+        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
+        session.setAttribute("Innlogget", "12345678901");
+
+        // act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","HeiHeiHei");
+        // assert
+        assertEquals("OK", resultat);
+    }
+}
