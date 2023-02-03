78b149d5429facaca705138437ae4162d6097f76
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2023-02-03 13:12:09 +0100

    enhetstest av admin konto controller, har fått kjørt gjennom to tester.

--- src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import oslomet.testing.API.AdminKontoController;
-import oslomet.testing.API.BankController;
+import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
@@ -26,7 +27,7 @@ public class EnhetstestAdminKontoController {
 
     @Mock
     //denne skal Mock'es, vi later som at vi kaller den eller den skal simuleres
-    private AdminKontoController repository;
+    private AdminRepository repository;
 
     //denne skal Mock'es - simulere kallet
     @Mock
@@ -37,18 +38,18 @@ public class EnhetstestAdminKontoController {
     public void hentAlleKonti_LoggetInn() {
         //arrange - må sette opp kunden/kunder før vi fortsetter
 
+        List<Konto> kontoinfo = new ArrayList<>(); //oppretter en ny arraylist
         //siden hent kontoinformasjon returnerer en list med info, tar vi
         //å lage en kunde liste eller flere
         Konto konti1 = new Konto("93840283962","39482101038", 500,
                 "Brukskonto", "NOK", null);
         Konto konti2 = new Konto("67840283940","56482101011", 1000,
                 "Lønnskonto", "NOK", null);
-        List<Konto> kontoinfo = new ArrayList<>(); //oppretter en ny arraylist
         kontoinfo.add(konti1);
         kontoinfo.add(konti2);
 
         //sjekker om sikkerhet først
-        when(sjekk.loggetInn()).thenReturn("93840283962");
+        when(sjekk.loggetInn()).thenReturn("84840323962");
 
         //sjekker for repository - skal kun returnere kontoinfo eller listen
         when(repository.hentAlleKonti()).thenReturn(kontoinfo);
@@ -56,10 +57,10 @@ public class EnhetstestAdminKontoController {
         //act - kjører løsningen
         //returnere en liste av kontoinfo, fra Bank controlleren
 
-        List<Konto> resultat = BankController.hentKonti();
+        List<Konto> resultat = adminKontoController.hentAlleKonti();
 
         //assert
-        assertEquals(kontoinfo, resultat);
+       assertEquals(kontoinfo, resultat);
     }
 
     @Test
@@ -69,14 +70,22 @@ public class EnhetstestAdminKontoController {
 
         //act - kjører løsningen
 
-        List<Konto> resultat = BankController.hentKonti();
+        List<Konto> resultat = adminKontoController.hentAlleKonti();
 
         //assert
-        assertNull(resultat);
+       assertNull(resultat);
     }
 
     @Test
     public void registrerKonto_loggetInn(){
+        //arrange
+        when(repository.registrerKonto()).thenReturn("OK");
+
+        //act
+
+
+
+        //assert
 
     }
 
