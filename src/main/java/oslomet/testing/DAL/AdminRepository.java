f0f2ed0a3be725691eb76268533e21a06c9b04e5
mbhira7 <bhiravina@hotmail.com>
2023-03-08 15:23:28 +0100

    Init DB

--- src/main/java/oslomet/testing/DAL/AdminRepository.java
+++ src/main/java/oslomet/testing/DAL/AdminRepository.java
@@ -1,11 +1,16 @@
 package oslomet.testing.DAL;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Repository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Models.Kunde;
+
+import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
@@ -163,5 +168,18 @@ public class AdminRepository {
             return "Feil kononummer";
         }
         return "OK";
+    }
+
+    public String initDB(DataSource dataSource){
+        try{
+            Resource skjema = new ClassPathResource("schema.sql");
+            Resource data = new  ClassPathResource("data.sql");
+            ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(skjema,data);
+            databasePopulator.execute(dataSource);
+            return "OK";
+        }
+        catch(Exception e){
+            return "Feil";
+        }
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@
 --
 
 INSERT INTO `Konto` (`Kontonummer`, `Personnummer`, `Saldo`, `Type`, `Valuta`) VALUES
-('105010123456', '01010110523', 720, 'Lønnskonto', 'NOK'),
+('105010123456', '01010110523', 720, 'Lonnskonto', 'NOK'),
 ('105020123456', '01010110523', 100500, 'Sparekonto', 'NOK'),
 ('22334412345', '01010110523', 10234.5, 'Brukskonto', 'NOK');
 
